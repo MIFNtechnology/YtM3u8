6bee5b1f1923547f196cb41a0a12707cb72a9a9d
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-03 00:18:02 +0800

    update bernama_m3ugrabber.py

--- scripts/bernama_m3ugrabber.py
+++ scripts/bernama_m3ugrabber.py
@@ -9,18 +9,18 @@ if 'win' in sys.platform:
     windows = True
 
 def grab(url):
-    response = requests.get(url, timeout=15).text
+    response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
-        #response = requests.get(url).text
+        response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/MIFNtechnology/html/YtM3u8/assets/multi_conn.m3u8')
+                print('https://raw.githubusercontent.com/MIFNtechnology/html/YtM3u8/assets/info.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/MIFNtechnology/YtM3u8/main/assets/multi_conn.m3u8')
+                print('https://raw.githubusercontent.com/MIFNtechnology/html/YtM3u8/assets/info.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
@@ -32,10 +32,16 @@ def grab(url):
             break
         else:
             tuner += 5
-    print(f"{link[start : end]}")
+    streams = s.get(link[start:end]).text.split('#EXT')
+    hd = streams[-1].strip()
+    st = hd.find('http')
+    print(hd[st:].strip())
+    #print(f"{link[start : end]}")
 
-print('#EXTM3U ')
-#s = requests.Session()
+print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+s = requests.Session()
 with open('../bernama_info.txt') as f:
     for line in f:
         line = line.strip()
@@ -47,10 +53,9 @@ with open('../bernama_info.txt') as f:
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
         else:
             grab(line)
-            
+
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
     os.system('rm watch*')
