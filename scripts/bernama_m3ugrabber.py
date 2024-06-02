c82c643198d2f4307342072cf07781f25e5c785d
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-02 23:32:46 +0800

    update bernama_m3ugrabber.py

--- scripts/bernama_m3ugrabber.py
+++ scripts/bernama_m3ugrabber.py
@@ -14,13 +14,13 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/MIFNtechnology/html/YtM3u8/assets/moose_na.m3u')
+                print('https://raw.githubusercontent.com/MIFNtechnology/html/YtM3u8/assets/multi_conn.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/MIFNtechnology/YtM3u8/main/assets/moose_na.m3u')
+                print('https://raw.githubusercontent.com/MIFNtechnology/YtM3u8/main/assets/multi_conn.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
