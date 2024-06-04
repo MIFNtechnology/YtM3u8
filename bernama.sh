373ca5f787ec6080714c9c026bef0f44f655fa9f
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-04 09:56:24 +0800

    Update bernama.sh
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- bernama.sh
+++ bernama.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 bernama_m3ugrabber.py > ../BernamaTv.m3u8
+python3 bernama_m3ugrabber.py > ../live/BernamaTv.m3u8
 
 echo m3u grabbed
