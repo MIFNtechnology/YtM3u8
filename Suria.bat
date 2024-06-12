1799f1374a67bf08b8c9e88b05125a121967cadf
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-12 13:56:46 +0800

    Update and rename Suria.bat to SuriaFm.bat
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- Suria.bat
+++
@@ -1,4 +0,0 @@
-pip install requests
-
-cd scripts/
-python Suria_m3ugrabber.py > ../live/Suria.m3u8
---
+++ SuriaFm.bat
@@ -0,0 +1,4 @@
+pip install requests
+
+cd scripts/
+python SuriaFm_m3ugrabber.py > ../live/SuriaFm.m3u8
