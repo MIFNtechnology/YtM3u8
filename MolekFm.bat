9ea1b384e2581179bc560b22965199fb3797c0e3
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-18 04:27:13 +0800

    Update and rename MolekFm.bat to MolekFm/autorun.bat
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- MolekFm.bat
+++
@@ -1,4 +0,0 @@
-pip install requests
-
-cd scripts/
-python MolekFm_m3ugrabber.py > ../live/MolekFm.m3u8
---
+++ MolekFm/autorun.bat
@@ -0,0 +1,4 @@
+pip install requests
+
+cd scripts/
+python MolekFm_m3ugrabber.py > ../live.m3u8
