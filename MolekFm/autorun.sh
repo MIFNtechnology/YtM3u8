a67acf87c97f0e97ce024742b0d27063f4160e0c
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-24 14:07:21 +0800

    Update autorun.sh
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- MolekFm/autorun.sh
+++ MolekFm/autorun.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 MolekFm_m3ugrabber.py > https://mifntechnology.github.io/siaranMy/radio/MolekFm/live.m3u8
+python3 MolekFm_m3ugrabber.py > https://mifntechnology.github.io/siaranMy/radio/MolekFm/playlist.m3u8
 
 echo m3u grabbed
