7cfda64bd864ce99b50a8c90388d8c0b95544a45
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-07-03 04:38:16 +0800

    Update and rename autorun.sh to autorun.sh
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- DidiDanFriends/autorun.sh
+++ DidiDanFriends/autorun.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 Didi&Friends_m3ugrabber.py > ../live.m3u8
+python3 DidiDanFriends_m3ugrabber.py > ../live.m3u8
 
 echo m3u grabbed
