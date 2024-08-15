7937f18051156eaf8c9a49ffd75eea9df637b832
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-08-15 18:56:25 +0800

    Update HotFm_m3ugrabber.py
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- HotFm/scripts/HotFm_m3ugrabber.py
+++ HotFm/scripts/HotFm_m3ugrabber.py
@@ -14,13 +14,13 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/MIFNtechnology/YtM3u8/github-private/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/MIFNtechnology/YtM3u8/github-private/assets/moose_na.m3u')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/MIFNtechnology/YtM3u8/github-private/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/MIFNtechnology/YtM3u8/github-private/assets/moose_na.m3u')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
